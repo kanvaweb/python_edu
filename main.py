a2383b82275ef3133bbe3dfb9ad96af73cc8ec63
kanvaweb <kanvaweb@gmail.com>
2022-07-21 21:42:09 +0700

    Шахматная доска

--- main.py
+++ main.py
@@ -68,26 +68,48 @@ import random
 # # ИГРА Камень - Ножницы - Бумага - ОКОНЧАНИЕ
 # #########################################
 
+# #########################################
+# # Задача с числом - НАЧАЛО
+# vved_znach = str(input("Введите число: "))
+# summa = 0 # Сумма всех цифр
+# proizv = 1 # Произведение всех цифр
+# col_zerro = 0 # Количество нулей
+# sred = 0 # Среднее арифметическое
+# col_nums = 0 # Количество цифр в числе
+# for cur_num in vved_znach:
+#     col_nums = col_nums + 1
+#     summa = summa + int(cur_num)
+#     proizv = proizv * int(cur_num)
+#     if int(cur_num) == 0:
+#         col_zerro = col_zerro + 1
+#
+# sred = summa / col_nums
+# print(f"Сумма всех цифр числа {vved_znach} равна {summa}")
+# print(f"Произведение всех цифр числа {vved_znach} равно {proizv}")
+# print(f"Количество нулей в числе {vved_znach} равно {col_zerro}")
+# print(f"Среднее арифметическое цифр в числе {vved_znach} равно {sred}")
+#
+# # Задача с числом - ОКОНЧАНИЕ
+# #########################################
+
 #########################################
-# Задача с числом - НАЧАЛО
-vved_znach = str(input("Введите число: "))
-summa = 0 # Сумма всех цифр
-mnoj = 1 # Умножение всех цифр
-col_zerro = 0 # Количество нулей
-sred = 0 # Среднее арифметическое
-col_nums = 0 # Количество цифр в числе
-for cur_num in vved_znach:
-    col_nums = col_nums + 1
-    summa = summa + int(cur_num)
-    mnoj = mnoj * int(cur_num)
-    if int(cur_num) == 0:
-        col_zerro = col_zerro + 1
+# Шахматная доска - НАЧАЛО
+razmer = int(input("Введите размер клетки: "))
 
-sred = summa / col_nums
-print(f"Сумма всех цифр числа {vved_znach} равна {summa}")
-print(f"Умножение всех цифр числа {vved_znach} равно {mnoj}")
-print(f"Количество нулей в числе {vved_znach} равно {col_zerro}")
-print(f"Среднее арифметическое цифр в числе {vved_znach} равно {sred}")
+for i in range(1, 9):
+    for j in range(1, 5):
+        if i % 2 == 0:
+            symbol1 = "-"
+            symbol2 = "*"
+        else:
+            symbol1 = "*"
+            symbol2 = "-"
+        for K in range(1, razmer + 1):
+            for z in range(1, razmer + 1):
+                print(symbol1, end=' ')
+            for z in range(1, razmer + 1):
+                print(symbol2, end=' ')
+        print("")
 
-# Задача с числом - ОКОНЧАНИЕ
+# Шахматная доска - ОКОНЧАНИЕ
 #########################################
