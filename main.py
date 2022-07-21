fdc90e3add82470036f30c663b63b1571714426d
kanvaweb <kanvaweb@gmail.com>
2022-07-21 20:00:24 +0700

    прерывание цикла

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 
-largest_number  = -999999999
-number = int(input("Введите число или напишите -1, чтобы остановить программу: "))
-while number != -1:
-    if number > largest_number:
-        largest_number = number
-    number = int(input("Введите число или напишите -1, чтобы остановить программу: "))
-print("Наибольшее число из введённых: ", largest_number)
+# largest_number  = -999999999
+# number = int(input("Введите число или напишите -1, чтобы остановить программу: "))
+# while number != -1:
+#     if number > largest_number:
+#         largest_number = number
+#     number = int(input("Введите число или напишите -1, чтобы остановить программу: "))
+# print("Наибольшее число из введённых: ", largest_number)
+
+# pow = 1
+# for exp in range(16):
+#     print(f"2 в {exp} степени равно {pow}")
+#     pow *= 2
 
 pow = 1
-for exp ing range(16):
+for exp in range(1,16):
+    if exp >= 3:
+        # Прервать цикл
+        break
     print(f"2 в {exp} степени равно {pow}")
     pow *= 2
