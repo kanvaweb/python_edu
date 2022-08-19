3dc4c5f42955353c39f2a416f49aed184ec64597
kanvaweb <kanvaweb@gmail.com>
2022-08-19 08:48:29 +0700

    work4

---
+++ work4.py
@@ -0,0 +1,14 @@
+import string
+
+formatter = string.Formatter()
+print(formatter.format('{userLog}', userLog = 'Admin'))
+print(formatter.format('{} {userName}', userName = 'Admin'))
+print(formatter.format(f'{userRights}', userRights = 'RIGHTS'))
+
+
+t=string.Template('$userName has the rights to $userRights in the $appName')
+
+str = t.substitute(userName = "adm",
+                userRights="full",
+                appName="Prog")
+print(str)
